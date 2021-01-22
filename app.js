87243a3a08d528e341463916c2fdd2615493d120
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-01-22 16:13:54 +0800

    optimize trash talk generator

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ const generateTrashTalk = require('./generate_trash_talk')
 const app = express()
 const port = 3000
 
+const occupations = [
+  {
+    'chineseTitle': '工程師',
+    'englishTitle': 'engineer',
+    'image': 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5668/angry-developer.jpg',
+    'id': '1'
+  },
+  {
+    'chineseTitle': '設計師',
+    'englishTitle': 'designer',
+    'image': 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5667/angry-designer.jpg',
+    'id': '2'
+  },
+  {
+    'chineseTitle': '創業家',
+    'englishTitle': 'entrepreneur',
+    'image': 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5669/angry-founder.jpg',
+    'id': '3'
+  }
+]
+
+
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -15,15 +37,16 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // setting routes
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { occupations })
 })
 
 app.post('/', (req, res) => {
-  const talk = generateTrashTalk(req.body)
-  res.render('index', { talk })
+  const option = req.body
+  const trashTalk = generateTrashTalk(option)
+  res.render('index', { occupations, trashTalk })
 })
 
 // starts the express server and listening for connections.
 app.listen(port, () => {
-  console.log(`Express app listening on http://localhost: ${port}.`)
+  console.log(`Express app listening on http://localhost: ${port}`)
 })
--- generate_trash_talk.js
+++ generate_trash_talk.js
@@ -1,39 +1,18 @@
-function sample(array) {
+//產生隨機抽取
+function generateRandomIndex(array) {
   const index = Math.floor(Math.random() * array.length)
   return array[index]
 }
 
-function generateTrashTalk(options) {
-
-  // const options = {
-  //   gridRadios: '設計師',
-  // }
-
+function generateTrashTalk(option) {
   const task = {
-    engineer: ['加個按鈕', '加新功能', '切個版', '改一點 code '],
-    designer: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計'],
-    entrepreneur: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
+    工程師: ['加個按鈕', '加新功能', '切個版', '改一點 code '],
+    設計師: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計'],
+    創業家: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
   }
-
   const phrase = ['很簡單', '很容易', '很快', '很正常']
-
-  let collection = []
-  if (options.gridRadios === '工程師') {
-    collection = collection.concat(task.engineer)
-  }
-  if (options.gridRadios === '設計師') {
-    collection = collection.concat(task.designer)
-  }
-  if (options.gridRadios === '創業家') {
-    collection = collection.concat(task.entrepreneur)
-  }
-
-  //抽取詞語並組合
-  let talk = [`${options.gridRadios}`]
-  talk += sample(collection)
-  talk += sample(phrase)
-  return talk
-
+  let trashTalk = `身為一個${option.job}，${generateRandomIndex(task[option.job])}，${generateRandomIndex(phrase)}吧！`
+  return trashTalk
 }
 
 module.exports = generateTrashTalk
